13f6e02165904cd67b9e85f138c4de2866d3de9b
@correiarangel <correiarangel@bol.com.br>
2019-07-31 11:26:03 -0300

    actInfo

---
+++ app/src/main/java/com/comeze/rangelti/hashisushiadmin/views/actInfo.java
@@ -0,0 +1,15 @@
+package com.comeze.rangelti.hashisushiadmin.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.comeze.rangelti.hashisushiadmin.R;
+
+public class actInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_info);
+    }
+}
